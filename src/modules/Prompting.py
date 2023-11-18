d4f845655e3043081826f83e29b8e01c431cc0ca
nutax <jose.huby@utec.edu.pe>
2023-11-18 15:19:32 -0500

    prompter

--- src/modules/Prompting.py
+++ src/modules/Prompting.py
@@ -3,17 +3,28 @@ from src.interfaces.Module import Module
 class Prompter(Module):
     def __init__(self, mode, next = None):
         super().__init__(next)
+        self.module = mode
         if mode == "simple":
-            self.purpose = "Resume el texto de entrada en puntos clave."
+            self.purpose = "TAREA: Resume el texto de entrada en puntos clave."
         elif mode == "verbose":
-            self.purpose = "Analiza la transcripción y señala los tópicos específicos que fueron discutidos con mayor énfasis en puntos clave."
+            self.purpose = "TAREA: Analiza la transcripción y señala los tópicos específicos que fueron discutidos con mayor énfasis en puntos clave."
         elif mode == "autocritic":
+            self.purpose = "TAREA: Analiza el texto inicial y reescribe los topicos detectados, añade temas de ser necesario."
             raise Exception("Mode not implemented")
         else:
             raise Exception("Invalid mode")
 
     def execute(self, text):
-        return {
-            "purpose": self.purpose,
-            "body": text
-        }
+        if self.module == "autocritic":
+            return {
+                "purpose": self.purpose,
+                "body": f"""
+                * TEXTO INICIAL: {text['ctx']['initial']}
+                * TOPICOS DETECTADOS: {text['prev']}
+"""
+            }
+        else:
+            return {
+                "purpose": self.purpose,
+                "body": text
+            }
